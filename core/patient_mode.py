c082f073574d2c6def1861595eecb7f41cc108b5
ahmedalijamil <ahmedalijamil87@gmail.com>
2026-09-13 08:26:49 +0500

    Create patient_mode.py

---
+++ core/patient_mode.py
@@ -0,0 +1,34 @@
+👤 Patient Mode
+Provide only information you are comfortable sharing.
+
+Age
+
+18
+
+
+Weight (kg)
+
+60.00
+
+
+Sex or gender (optional)
+
+Height (cm)
+
+170.00
+
+
+Existing medical conditions
+
+Current/recent medications
+
+Allergies
+
+Important medical history
+
+
+What symptoms or concerns are you experiencing?
+
+
+How long have you had them?
+
